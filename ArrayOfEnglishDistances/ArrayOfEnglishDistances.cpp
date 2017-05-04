0f7dcb53b0ffaff0297ef5bd5d02a42ab9378388
Tom Breza <tom@pcservicegroup.co.uk>
2017-05-04 14:46:08 +0100

    protection of array not to go over certain size!

--- ArrayOfEnglishDistances/ArrayOfEnglishDistances.cpp
+++ ArrayOfEnglishDistances/ArrayOfEnglishDistances.cpp
@@ -7,14 +7,22 @@
 
 int main()
 {
-	Distance dist[100]; // array of distances
+	const int MAX = 4;
+	Distance dist[MAX]; // array of distances
 	int n = 0; // count the entries
 	char ans; // user response 'y' or 'n'
 
-	do { std::cout << "Enter distance number " << n + 1 << " "; // get distance form user
-	dist[n++].getDistance(); // store distance in array
-	std::cout << "Do you want provide another distance (y/n)?"; 
-	std::cin >> ans;
+
+	do { 
+		if (n >= MAX)
+		{
+			std::cout << "Array is full!!!!!" << std::endl;
+			break;
+		}
+			std::cout << "Enter distance number " << n + 1 << " "; // get distance form user
+			dist[n++].getDistance(); // store distance in array
+			std::cout << "Do you want provide another distance (y/n)?"; 
+			std::cin >> ans;
 	} while (ans != 'n' && ans != 'N'); //quit if user types n
 	
 	for (int j = 0; j < n; j++) // display distances
--- ArrayOfEnglishDistances/Distance.cpp
+++ ArrayOfEnglishDistances/Distance.cpp
@@ -10,7 +10,7 @@ void Distance::getDistance() // get lenght from user
 	std::cout << "Enter Feet: "; std::cin >> feet;
 	std::cout << "Enter Inches: "; std::cin >> inches;
 }
-void Distance::showDistance() //display distance
+void  Distance::showDistance()//display distance
 {
 	std::cout << feet << "\'- " << inches << "\" ";
 }
